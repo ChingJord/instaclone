442a478ae534eafcf8879c06c6bfa61ce1f3cf5b
Jordan Ching <ChingJord@gmail.com>
2016-01-22 14:17:27 +1300

    create controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,13 @@ class PostsController < ApplicationController
 
   def create
   	@post Post.new(permit_params)
+  	if @post.save
+  		flash[:success] = "Success!"
+  		redirect_to show_post_path(@post)
+  	else
+  		flash[:error] = @post.errors.full_messages
+  		redirect_to new_post_path
+  	end
   end
 
   private
